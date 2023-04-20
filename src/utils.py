a3ec7e7ad33bf98b0fa5c962fa329a896bcb48aa
Shubhangi Sakarkar <shubhangi.s@tataelxsi.co.in>
2023-04-20 11:23:27 +0530

    Flask API successfully created

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score 
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -15,12 +17,18 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evalute_model(X_train,y_train,X_test,y_test,models):
+def evalute_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            #model.fit(X_train,y_train)
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             y_predict_train=model.predict(X_train)
             y_predict_test=model.predict(X_test)
@@ -31,6 +39,14 @@ def evalute_model(X_train,y_train,X_test,y_test,models):
             report[list(models.keys())[i]]=testing_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return  pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
                
